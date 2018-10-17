cca6a9d854acee429bb26cdbbd63dfdedc636ea7
DConfer99 <dillonconfer@gmail.com>
2018-10-17 09:07:22 -0400

    test

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
     <link href="signin.css" rel="stylesheet">
   </head>
 
-  <body class="text-center">
+  <body class="text-center" style="background-image:images/greenhill.com">
     <form class="form-signin">
       <img src="images/pickle.png" alt="" width="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
