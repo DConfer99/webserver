15c2345e1e98fcf9c4f02fcafe292bbcb71be935
DConfer99 <dillonconfer@gmail.com>
2018-10-17 21:22:42 -0400

    test

--- login.php
+++ login.php
@@ -31,10 +31,10 @@
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-    </div>
       <button class="btn btn-primary btn-lg btn-block" type="submit" style="background-color:green; border-color:darkGreen">Sign in</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
+    </div>
   </div>
   </body>
 </html>
